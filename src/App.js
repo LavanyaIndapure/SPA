3e3cdcb75695fb5c8ff97367945d22feff27a186
Lavanya <dlavanyaindapure@gmail.com>
2023-09-30 11:26:54 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+const ProfileCard = ({ imageSrc, profession, price, name, address, about, status }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="p1">
+        <img src={imageSrc} alt="" />
+        <p id="profession">{profession}</p>
+        <p id="price">{price}</p>
+      </div>
+      <div className="p2">
+        <div className="info">
+          <p id="name">{name}</p>
+          <p id="address">
+            <i className="fa-sharp fa-solid fa-location-dot"></i>
+            {address}
+          </p>
+          <p id="about">{about}</p>
+
+          <div className="buttons">
+            <button id="viewcv">VIEW CV</button>
+            <br />
+            <button id="makeoffer">MAKE OFFER</button>
+          </div>
+
+          <p className="status">{status}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+const UserProfileCards = () => {
+  return (
+    <div className="container">
+      <ProfileCard
+        imageSrc="Images/image5.jpg"
+        profession="Film Director"
+        price="$14/hour"
+        name="Ashutosh Indapure,51"
+        address="New York, United States"
+        about="Ashutosh was born in New York and brought up in Los Angeles"
+        status={<><i className="fa-solid fa-circle" style={{ color: '#0cf708' }}></i> Online</>}
+      />
+
+      <ProfileCard
+        imageSrc="Images/image-Kathy.png"
+        profession="Actor, Film Director"
+        price="$9/hour"
+        name="Baltasar Kormakur, 52"
+        address="Reykjavik, Iceland"
+        about="Iceland actor, theater and film director and film producer."
+        status="Last seen: 22 minutes ago"
+      />
+
+      <ProfileCard
+        imageSrc="Images/image6.png"
+        profession="Actor, Singer"
+        price="$12/hour"
+        name="Daniel Abraham, 48"
+        address="Canberra, Australia"
+        about="Brilliant Actor and Outstanding Singer."
+        status="Seen long time ago"
+      />
+    </div>
+  );
+};
+
+export default UserProfileCards;
